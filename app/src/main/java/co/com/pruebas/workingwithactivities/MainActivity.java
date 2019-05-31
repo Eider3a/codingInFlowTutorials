599bb5becee7b56d2785fe701a18cb4b1cb97214
Eidera <eider.udea@gmail.com>
2019-05-31 17:35:01 -0500

    First Commit

---
+++ app/src/main/java/co/com/pruebas/workingwithactivities/MainActivity.java
@@ -0,0 +1,17 @@
+package co.com.pruebas.workingwithactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //This project is about working with activities as well working with recyclerView.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
